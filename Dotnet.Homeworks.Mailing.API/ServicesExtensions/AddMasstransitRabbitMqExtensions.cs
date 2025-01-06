234a95dcdc7563f06c23f0119fe7b173f49a60c1
Aysulu Gataullina <aysulu.gataullina@truesolv.com>
2025-01-07 02:51:38 +0300

    feat: homework 5

--- Dotnet.Homeworks.Mailing.API/ServicesExtensions/AddMasstransitRabbitMqExtensions.cs
+++ Dotnet.Homeworks.Mailing.API/ServicesExtensions/AddMasstransitRabbitMqExtensions.cs
@@ -1,4 +1,7 @@
 using Dotnet.Homeworks.Mailing.API.Configuration;
+using Dotnet.Homeworks.Mailing.API.Helpers;
+using MassTransit;
+using Microsoft.Extensions.Options;
 
 namespace Dotnet.Homeworks.Mailing.API.ServicesExtensions;
 
@@ -7,6 +10,20 @@ public static class AddMasstransitRabbitMqExtensions
     public static IServiceCollection AddMasstransitRabbitMq(this IServiceCollection services,
         RabbitMqConfig rabbitConfiguration)
     {
-        throw new NotImplementedException();
+        return services.AddMassTransit(x =>
+        {
+            x.SetKebabCaseEndpointNameFormatter();
+            x.AddConsumers(AssemblyReference.Assembly);
+            
+            x.UsingRabbitMq((context, cfg) =>
+            {
+                cfg.Host(rabbitConfiguration.Hostname, h =>
+                {
+                    h.Username(rabbitConfiguration.Username);
+                    h.Password(rabbitConfiguration.Password);
+                });
+                cfg.ConfigureEndpoints(context);
+            });
+        });
     }
 }
