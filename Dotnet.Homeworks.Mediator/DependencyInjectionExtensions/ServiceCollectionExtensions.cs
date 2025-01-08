6abd3ec6eadc8d8c344d2da0ba4f6bf6af066b39
Aysulu Gataullina <aysulu.gataullina@truesolv.com>
2025-01-08 17:46:13 +0300

    fix: add handlers registration

--- Dotnet.Homeworks.Mediator/DependencyInjectionExtensions/ServiceCollectionExtensions.cs
+++ Dotnet.Homeworks.Mediator/DependencyInjectionExtensions/ServiceCollectionExtensions.cs
@@ -1,13 +1,50 @@
 using System.Reflection;
+using Dotnet.Homeworks.Mediator.Helpers;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Dotnet.Homeworks.Mediator.DependencyInjectionExtensions;
 
 public static class ServiceCollectionExtensions
 {
-    //TODO: Register your custom mediator
     public static IServiceCollection AddMediator(this IServiceCollection services, params Assembly[] handlersAssemblies)
     {
-        throw new NotImplementedException();
+        // Ключ - тип обрабатываемого запроса
+        // Значение - обработчик
+        Dictionary<Type, Type> handlerTypes = [];
+        
+        foreach (var handlerAssembly in handlersAssemblies)
+            foreach (var handlerType in handlerAssembly.GetTypes())
+                foreach (var handlerInterface in handlerType.GetInterfaces())
+                {
+                    if (!handlerInterface.IsGenericType) continue;
+                    
+                    var genericDefinition = handlerInterface.GetGenericTypeDefinition();
+                    if (genericDefinition != typeof(IRequestHandler<>)
+                        && genericDefinition != typeof(IRequestHandler<,>)) continue;
+                    
+                    services.AddScoped(handlerInterface, handlerType);
+                    var genericArguments = handlerInterface.GetGenericArguments();
+                    var requestType = genericArguments[0];
+                        
+                    // в текущей реализации на один тип запроса не может быть 
+                    // больше одного обработчика
+                    if (!handlerTypes.TryAdd(requestType, handlerType))
+                    {
+                        throw new InvalidOperationException($"Multiple handlers found for request type {requestType.FullName}");
+                    }
+                }
+
+        services.AddSingleton<IMediator, Mediator>(provider =>
+        {
+            var mediator = new Mediator(provider);
+            foreach (var handlerType in handlerTypes)
+            {
+                mediator.Register(handlerType.Key, handlerType.Value);
+            }
+
+            return mediator;
+        });
+        
+        return services;
     }
 }
---
+++ Dotnet.Homeworks.Mediator/Mediator.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Concurrent;
+
+namespace Dotnet.Homeworks.Mediator;
+
+public class Mediator(IServiceProvider serviceProvider) : IMediator
+{
+    private readonly ConcurrentDictionary<Type, Type> _requestHandlers = new();
+
+    public void Register(Type requestType, Type handlerType)
+    {
+        _requestHandlers.TryAdd(requestType, handlerType);
+    }
+    
+    public async Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
+    {
+        if (request == null) throw new ArgumentNullException(nameof(request));
+        
+        var handler = _requestHandlers.GetValueOrDefault(request.GetType());
+        if (handler == null)
+        {
+            throw new InvalidOperationException($"Handler for '{request.GetType().Name}' not found.");
+        }
+
+        return await ((IRequestHandler<IRequest<TResponse>, TResponse>) handler)
+            .Handle(request, cancellationToken);
+    }
+
+    public async Task Send<TRequest>(TRequest request, CancellationToken cancellationToken = default) where TRequest : IRequest
+    {
+        if (request == null) throw new ArgumentNullException(nameof(request));
+        
+        var handler = _requestHandlers.GetValueOrDefault(request.GetType());
+        if (handler == null)
+        {
+            throw new InvalidOperationException($"Handler for '{request.GetType().Name}' not found.");
+        }
+
+        await ((IRequestHandler<IRequest>) handler).Handle(request, cancellationToken);
+    }
+
+    public async Task<dynamic?> Send(dynamic request, CancellationToken cancellationToken = default)
+    {
+        if (request == null)
+        {
+            throw new ArgumentNullException(nameof(request));
+        }
+
+        if (request is not IRequest<dynamic> requestTyped)
+        {
+            throw new ArgumentNullException(nameof(request), "Invalid request type");
+        }
+        
+        var handler = _requestHandlers.GetValueOrDefault(requestTyped.GetType());
+        if (handler == null)
+        {
+            throw new InvalidOperationException($"Handler for '{request.GetType().Name}' not found.");
+        }
+
+        return await ((IRequestHandler<IRequest<dynamic>, dynamic>) handler)
+            .Handle(request, cancellationToken);
+    }
+}
